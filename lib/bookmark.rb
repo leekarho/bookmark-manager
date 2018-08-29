dfcfdc9ec24ad3c97bed8098ea26e385eaedec09
leekarho <k-h_lee@hotmail.co.uk>
2018-08-29 22:41:10 +0100

    required pg

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,16 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "http://www.google.com",
-      "http://www.bbc.co.uk"
-     ]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,9 +3,14 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+
       bookmarks = Bookmark.all
+      
       expect(bookmarks).to include(("http://www.google.com"))
-      expect(bookmarks).to include(("http://www.bbc.co.uk"))
+      expect(bookmarks).to include(("http://www.destroyallsoftware.com"))
     end
   end
 end
